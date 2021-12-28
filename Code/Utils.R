6dd0e770e02f2635fbd9833edf206083152fea35
nkurniansyah <nuzulul.kurniansyah1992@gmail.com>
2021-12-28 14:53:27 -0500

    First initial

---
+++ Code/Utils.R
@@ -0,0 +1,49 @@
+#' Run association test
+#'
+#' @param pheno : data frame of the phenotype 
+#' @param outcome : as.numeric , outcome to test 
+#' @param covars_prs : covariates to adjust
+#' @param group.var : 
+#' @param covmat: specifies the covariance structures for the random effects included in the model 
+#'
+#' @return association test results in data frame
+#' @export
+#'
+#' @examples
+#' 
+
+run_assoc_mixmodel<-function(pheno, outcome, covars_prs, covmat=NULL,group.var=NULL){
+  
+  
+  if(all(pheno[,outcome] %in% c(0,1,NA))){
+    family<-"binomial"
+  }else{
+    family="gaussian"
+  }
+  
+  nullmod_stage1 <- fitNullModel(pheno,
+                          outcome = outcome,
+                          covars = covars_prs,
+                          family = family,
+                          cov.mat=covmat,
+                          group.var = group.var,
+                          verbose=TRUE)
+  
+  nullmod_stage2<-nullModelInvNorm(nullmod_stage1, cov.mat=cov.mat,
+                                   norm.option="all",
+                                   rescale="residSD")
+  
+  
+  
+
+  nullmod_other_fixef<- nullmod_stage2$fixef
+  nullmod_other_fixef<-nullmod_other_fixef[grepl("prs",rownames(nullmod_other_fixef)),]
+  sample_size<-length(nullmod$outcome)
+  assoc_df<- data.frame(nullmod_other_fixef, sample_size)
+  colnames(assoc_df)<- c("prs_effect","prs_se","prs_stat","pval_assoc_with_outcome","sample_size")
+  assoc_df<- assoc_df %>% rownames_to_column(var="exposure")
+  
+  return(assoc_df)
+  
+}
+
