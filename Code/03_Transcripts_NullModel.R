2ba35f14ca84fcee33520ff30723d085dbfa1034
nkurniansyah <nuzulul.kurniansyah1992@gmail.com>
2021-12-28 12:26:05 -0500

    First Initial

---
+++ Code/03_Transcripts_NullModel.R
@@ -0,0 +1,63 @@
+library(data.table)
+library(dplyr)
+library(GENESIS)
+library(SeqArray)
+
+
+
+
+
+
+#We performed median normalization and log half min transcripts in RNA-seq counts, then we extract significant transcripts fdr_bh < 0.1.
+#finaly we merge with our phenotypes. 
+
+
+
+
+phenotype_file<-"./SDB_phenotype_with_transcripts_values.csv"
+
+
+pheno<- fread(phenotype_file, data.table = F)
+head(pheno)
+
+covars<-c("shipment","BroadUW","plate","age","sex","race","Study site","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10")
+
+outcome<-"FAM106A"
+
+
+cov.mat<- getobj("./pcrelate_kinshipMatrix_sparseDeg4.RData")
+
+
+
+pheno_annot<-AnnotatedDataFrame(pheno)
+
+nullmodel_stage1<-fitNullModel(pheno, 
+                              outcome= outcome, 
+                              covars=covars,
+                              group.var = "race",
+                              family = "gaussian", 
+                              cov.mat=cov.mat)
+
+
+
+message("Refitting model with inverse normal transformation.")
+message(length(nullmodel_stage1$outcome), " samples")
+
+nullmodel_stage2 <- nullModelInvNorm(nullmodel_stage1, cov.mat=cov.mat,
+                                    norm.option="all",
+                                    rescale="residSD")
+
+
+
+output_file<-"./FAM106A_nullmodel.RData"
+save(nullmodel_stage2, file = output_file)
+
+
+
+
+
+
+
+
+
+
